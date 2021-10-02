478c48ebb4d22cf0e7b2bb05f0458b7ff86f6673
Made Putra <madesusena96@gmail.com>
2021-10-03 00:53:11 +0800

    cleaning up canvas

--- user_interface.py
+++ user_interface.py
@@ -3,17 +3,6 @@ import tkinter as tk
 import subprocess as sub
 from processor import stringSeparator
 
-class Redirect():
-
-    def __init__(self, widget, autoscroll=True):
-        self.widget = widget
-        self.autoscroll = autoscroll
-
-    def write(self, text):
-        self.widget.insert('end', text)
-        if self.autoscroll:
-            self.widget.see("end") 
-
 root = tk.Tk()
 root.title("Matt's Word Sorter")
 
